5fa9295c9dae5316dbc20e529b5320eb6ebbb64e
stavdorani <85304499+stavdorani@users.noreply.github.com>
2022-05-19 23:46:43 +0300

    part 2 update

---
+++ static/myScript.js
@@ -0,0 +1,48 @@
+try {
+    const contant_header_con = document.getElementById("contant_header_con");
+    contant_header_con.addEventListener("mouseover", changeBorderStyle2);
+    contant_header_con.addEventListener("mouseleave", changeBorderColor);
+  } catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  }
+
+try {
+    const contant_header_hom = document.getElementById("contant_header");
+    const borderColor = contant_header_hom.style.borderColor;
+    contant_header_hom.addEventListener("mouseover", changeBorderStyle);
+    contant_header_hom.addEventListener("mouseleave", changeBorderColor);
+} catch (error) {
+console.error(error);
+// expected output: ReferenceError: nonExistentFunction is not defined
+// Note - error messages will vary depending on browser
+}
+
+function changeBorderStyle2(){
+    this.style.borderRadius = "15px" ;
+    this.style.borderStyle = "dotted";
+    this.style.color = "rgb(80, 86, 87)";
+}
+function changeBorderColor(){
+    this.style.color = "rgb(31, 195, 222)";
+}
+
+
+function changeBorderStyle(){
+    this.style.borderRadius = "15px" ;
+    this.style.borderStyle = "dashed";
+    this.style.color = "rgb(0, 78, 100)";
+}
+
+const navHom = document.getElementById("hom");
+const navCon = document.getElementById("con");
+
+if ( document.URL.includes("home%20page.html") ) {
+    navHom.style.color = "green";
+    navHom.style.fontSize = "25px";
+
+} else {
+    navCon.style.color = "green";
+    navCon.style.fontSize = "25px";
+}
